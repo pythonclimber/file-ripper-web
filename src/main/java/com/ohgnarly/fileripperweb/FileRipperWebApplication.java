76515d849d44c97e69255376d8b9f09b4f3d424c
Aaron <asmitty92@gmail.com>
2023-12-30 12:03:46 -0600

    Configure port

--- src/main/java/com/ohgnarly/fileripperweb/FileRipperWebApplication.java
+++ src/main/java/com/ohgnarly/fileripperweb/FileRipperWebApplication.java
@@ -3,9 +3,19 @@ package com.ohgnarly.fileripperweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
+import static java.lang.System.getenv;
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
 @SpringBootApplication
 public class FileRipperWebApplication {
   public static void main(String[] args) {
-    SpringApplication.run(FileRipperWebApplication.class, args);
+    SpringApplication app = new SpringApplication(FileRipperWebApplication.class);
+    String port = isNotBlank(getenv("PORT"))
+            ? getenv("PORT")
+            : "3000";
+    app.setDefaultProperties(Collections.singletonMap("server.port", port));
+    app.run(args);
   }
 }
